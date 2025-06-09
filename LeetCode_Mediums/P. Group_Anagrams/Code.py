320e64f594e25df83cb2b213943f53b0aa44f99b
ROHI155 <brohith519@gmail.com>
2025-06-09 19:24:51 +0530

    Group_Anagrams

---
+++ LeetCode_Mediums/P. Group_Anagrams/Code.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagrams = defaultdict(list) # Opening a empty dictoinary with default keys for empty words.
+        for i in strs: # loop to iterate through each element in the list.
+            sort = ''.join(sorted(i)) # Assign the sort as the values of i in sorted way and group them as a single element.
+            anagrams[sort].append(i) # append the  word i and the associated word sort as key to the dictionary.
+        return list(anagrams.values()) # Return the list values of the anagrams.
